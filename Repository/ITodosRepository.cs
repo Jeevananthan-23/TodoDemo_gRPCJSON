40c3cb483f5ddff4a029a06b20671af8865ad303
Jeevananthan-23 <jeeva023@outlook.com>
2023-02-22 16:50:44 +0530

    Add project files.

---
+++ Repository/ITodosRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoDemo_gRPCJSON.Repository
+{
+    public interface ITodosRepository
+    {
+        IEnumerable<(int id, string description)> GetTodos();
+        string GetTodo(int id);
+        void InsertTodo(string description);
+        void UpdateTodo(int id, string description);
+        void DeleteTodo(int id);
+    }
+}
